0f29bf9a65faa5a49c0215947e31b62f1689361b
mellowc0de <mellohcode@gmail.com>
2025-11-29 12:01:24 -0800

    Create gradio_basic.py

---
+++ gradio_basic.py
@@ -0,0 +1,18 @@
+from PIL import Image
+from transformers import AutoProcessor, BlipForConditionalGeneration
+
+# Load the pretrained processor and model
+processor = AutoProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
+
+# load image
+img_path = "image_3.jpeg" # Replace with image
+image = Image.open(img_path).convert('RGB')
+
+text = "the image of"
+inputs = processor(images=image, text=text, return_tensors="pt")
+
+outputs = model.generate(**inputs, max_length=50)
+
+caption = processor.decode(outputs[0], skip_special_tokens=True)
+print(caption)
